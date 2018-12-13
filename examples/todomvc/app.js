683fbd911e8d25a4e29d52b1a619d508b920e484
Pavel Martynov <martynov.pl@gmail.com>
2018-12-13 11:37:40 -0800

    Removed fragments example from todomvc

--- examples/todomvc/app.js
+++ examples/todomvc/app.js
@@ -66,12 +66,6 @@ const mainView = h`
             </footer>
         </section>
     </section>
-    <footer class="info">
-        <p>Double-click to edit a todo</p>
-        <p>Written by <a href="http://twitter.com/lukeed05">Luke Edwards</a></p>
-        <p>Refactored by <a href="https://github.com/xorgy">Aaron Muir Hamilton</a></p>
-        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-    </footer>
 `
 function Main(todos) {
     const root = mainView
